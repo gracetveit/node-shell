494436ce102b8def74051d59bdb668ef6f9dfea9
Grace Tveit <grace.tveit@gmail.com>
2021-08-06 13:18:49 -0700

    added JSDocs comment to cat.js

--- cat.js
+++ cat.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+/**
+ * prints to console the content of a given file
+ * @param {string} fileNa - Filename
+ */
 module.exports = function cat(fileNa) {
     fs.readFile(`./${fileNa}`, 'utf8', (err, data) => {
         if(err){
